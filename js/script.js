d13c07d73effb03ebbaa2d145b29be1a3ee5ab65
Jachob Westh <jachobwesth@gmail.com>
2022-06-14 20:10:20 +0200

    Alle sider lavet. om mig mangler

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 // laver variabler på knapper
-let webKnap = document.querySelector("#webKnap");
-let uxKnap = document.querySelector("#uxKnap");
-let animKnap = document.querySelector("#animKnap");
-let indKnap = document.querySelector("#indKnap");
-let pfKnap = document.querySelector("#pfKnap");
+var webKnap = document.querySelector("#webKnap");
+var uxKnap = document.querySelector("#uxKnap");
+var animKnap = document.querySelector("#animKnap");
+var indKnap = document.querySelector("#indKnap");
+var pfKnap = document.querySelector("#pfKnap");
 
 // laver variabler på sectioner
-let web = document.querySelector("#web");
-let ux = document.querySelector("#ux");
-let anim = document.querySelector("#animation");
-let ind = document.querySelector("#indhold");
-let pf = document.querySelector("#portfolio");
+var web = document.querySelector("#web");
+var ux = document.querySelector("#ux");
+var anim = document.querySelector("#animation");
+var ind = document.querySelector("#indhold");
+var pf = document.querySelector("#portfolio");
 
+// putter eventlistensers på knapper
 webKnap.addEventListener("click", webFourth);
 uxKnap.addEventListener("click", uxFourth);
 animKnap.addEventListener("click", animFourth);
 indKnap.addEventListener("click", indFourth);
 pfKnap.addEventListener("click", pfFourth);
 
+// funktioner
 function webFourth() {
   web.scrollIntoView({
     behavior: "smooth",
